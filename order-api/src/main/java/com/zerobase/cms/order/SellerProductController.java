a73d89993b67c4e5ff568c760dd8375378f6d822
zhu <parkjw573@naver.com>
2023-03-29 19:26:34 +0900

    feat : [009-product-add] product add, productItem add
    추가로 Exception에 대한 Response의 visuality 향상

--- order-api/src/main/java/com/zerobase/cms/order/SellerProductController.java
+++ order-api/src/main/java/com/zerobase/cms/order/SellerProductController.java
@@ -1,7 +1,9 @@
 package com.zerobase.cms.order;
 
 import com.zerobase.cms.order.domain.product.AddProductForm;
+import com.zerobase.cms.order.domain.product.AddProductItemForm;
 import com.zerobase.cms.order.domain.product.ProductDto;
+import com.zerobase.cms.order.service.ProductItemService;
 import com.zerobase.cms.order.service.ProductService;
 import com.zerobase.domain.config.JwtAuthenticationProvider;
 import lombok.RequiredArgsConstructor;
@@ -13,11 +15,18 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class SellerProductController {
     private final ProductService productService;
+    private final ProductItemService productItemService;
     private final JwtAuthenticationProvider provider;
 
     @PostMapping
     public ResponseEntity<ProductDto> addProduct(@RequestHeader(name = "X-AUTH-TOKEN") String token,
                                                  @RequestBody AddProductForm form) {
         return ResponseEntity.ok(ProductDto.from(productService.addProduct(provider.getUserVo(token).getId(), form)));
+    }
+
+    @PostMapping("/item")
+    public ResponseEntity<ProductDto> addProductItem(@RequestHeader(name = "X-AUTH-TOKEN") String token,
+                                                     @RequestBody AddProductItemForm form) {
+        return ResponseEntity.ok(ProductDto.from(productItemService.addProductItem(provider.getUserVo(token).getId(), form)));
     }
 }
